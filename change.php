377053ca2f23fca1c99da8a79d40ae53a900528e
Michael <michaeljin.lai@gmail.com>
2015-11-08 02:19:00 +1100

    YES PASSWORD RECOVERY WORKS (Live Only)

--- change.php
+++ change.php
@@ -30,11 +30,11 @@ if (isset($_POST["ForgotPassword"])) {
 		$dbpassword = hash('sha512', $salt.$userExists["email"]);
 
 		// Create a url which we will direct them to reset their password
-		$pwrurl = "www.yoursitehere.com/reset_password.php?q=".$dbpassword;
+		$pwrurl = "clashofclans.mikelai.org/reset_password.php?q=".$dbpassword;
 		
 		// Mail them their key
-		$mailbody = "Dear user,\n\nIf this e-mail does not apply to you please ignore it. It appears that you have requested a password reset at our website www.yoursitehere.com\n\nTo reset your password, please click the link below. If you cannot click it, please paste it into your web browser's address bar.\n\n" . $pwrurl . "\n\nThanks,\nThe Administration";
-		mail($userExists["email"], "www.yoursitehere.com - Password Reset", $mailbody);
+		$mailbody = "Dear user,\n\nIf this e-mail does not apply to you please ignore it. It appears that you have requested a password reset at our website clashofclans.mikelai.org\n\nTo reset your password, please click the link below. If you cannot click it, please paste it into your web browser's address bar.\n\n" . $pwrurl . "\n\nThanks,\nThe Administration";
+		mail($userExists["email"], "clashofclans.mikelai.org - Password Reset", $mailbody);
 		echo "Your password recovery key has been sent to your e-mail address.";
 		
 	}
@@ -42,7 +42,3 @@ if (isset($_POST["ForgotPassword"])) {
 		echo "No user with that e-mail address exists.";
 }
 ?>
-
-<form action="change.php" method="POST">
-E-mail Address: <input type="text" name="email" size="20" /> <input type="submit" name="ForgotPassword" value=" Request Reset " />
-</form>
--- reset.php
+++ reset.php
@@ -12,22 +12,27 @@ if (isset($_POST["ResetPasswordForm"]))
 	$hash = $_POST["q"];
 
 	// Use the same salt from the forgot_password.php file
-	$salt = "498#2D83B631%3800EBD!801600D*7E3CC13";
+	$resetsalt = "498#2D83B631%3800EBD!801600D*7E3CC13";
 
 	// Generate the reset key
-	$resetkey = hash('sha512', $salt.$email);
+	$resetkey = hash('sha512', $resetsalt.$email);
 
 	// Does the new reset key match the old one?
 	if ($resetkey == $hash)
 	{
 		if ($dbpassword == $confirmpassword)
 		{
-			//has and secure the password
-			$dbpassword = hash('sha512', $salt.$dbpassword);
+			$salt = dechex(mt_rand(0, 2147483647)) . dechex(mt_rand(0, 2147483647)); 
+			$password = hash('sha256', $_POST['password'] . $salt);
+            for($round = 0; $round < 65536; $round++) 
+            { 
+                $password = hash('sha256', $password . $salt); 
+            } 
 
 			// Update the user's password
-				$query = $db->prepare('UPDATE users SET password = :password WHERE email = :email');
-				$query->bindParam(':password', $dbpassword);
+				$query = $db->prepare('UPDATE users SET password = :password, salt = :salt WHERE email = :email');
+				$query->bindParam(':password', $password);
+				$query->bindParam(':salt', $salt);
 				$query->bindParam(':email', $email);
 				$query->execute();
 				$db = null;
