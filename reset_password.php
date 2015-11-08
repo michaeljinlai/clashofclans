0b7eb7d95d38c09141228f501eac6df1283738c1
Michael <michaeljin.lai@gmail.com>
2015-11-09 00:32:25 +1100

    reset_password bootstrap

--- reset_password.php
+++ reset_password.php
@@ -1,14 +1,42 @@
+<?php require('Elements/header.php');?>
 <?php echo '
-<form action="reset.php" method="POST">
-E-mail Address: <input type="text" name="email" size="20" /><br />
-New Password: <input type="password" name="password" size="20" /><br />
-Confirm Password: <input type="password" name="confirmpassword" size="20" /><br />
-<input type="hidden" name="q" value="';
-if (isset($_GET["q"])) {
-	echo $_GET["q"];
-}
-	echo '" /><input type="submit" name="ResetPasswordForm" value=" Reset Password " />
-</form>';
+<body>
+	<div class="container">
+		<div class="span10 offset1">
+			<div class="row">
+			<h3>Reset Password</h3>
+			</div>
+			<form class="form-horizontal" action="reset.php" method="POST">
+				<div class="control-group">
+					<label class="control-label">Email</label>
+					<div class="controls">
+						<input type="text" name="email" placeholder="Please enter your account email">
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label">Password</label>
+					<div class="controls">
+						<input type="password" name="password" placeholder="Password">
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label">Confirm Password</label>
+					<div class="controls">
+						<input type="password" name="confirmpassword" placeholder="Confirm Password">
+					</div>
+				</div>
+				<input type="hidden" name="q" value="';
+				if (isset($_GET["q"])) {
+					echo $_GET["q"];
+				}
+					echo '" />
+				<div class="form-actions">
+					<button type="submit" name="ResetPasswordForm" class="btn btn-success">Reset Password</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</body>';
 
 ?>
 
