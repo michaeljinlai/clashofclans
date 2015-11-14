0da3771bcebba37ba5440f3ce60b5b8c02257e92
Michael <michaeljin.lai@gmail.com>
2015-11-15 03:09:46 +1100

    Chat popup with no more password

--- private.php
+++ private.php
@@ -40,6 +40,7 @@ if(empty($_SESSION['user']) || $_SESSION['user']['privilege'] !== 'administrator
       <h1 class="page-header">Home</h1>
       <div>Hello <?php echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8'); ?>, secret content!</div><br /> 
       <a href="memberlist.php">Memberlist</a><br /> 
+      <a href="chat/" onclick="openWindow(this.href);this.blur();return false;">Chat</a>
   </div>
 </div>
 
@@ -52,4 +53,18 @@ $(document).ready(function(){
 });
 </script>
 
-
+<script type="text/javascript">
+  // <![CDATA[
+    function openWindow(url,width,height,options,name) {
+      width = width ? width : 800;
+      height = height ? height : 600;
+      options = options ? options : 'resizable=yes';
+      name = name ? name : 'openWindow';
+      window.open(
+        url,
+        name,
+        'screenX='+(screen.width-width)/2+',screenY='+(screen.height-height)/2+',width='+width+',height='+height+','+options
+      )
+    }
+  // ]]>
+</script>
