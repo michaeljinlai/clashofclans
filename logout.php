dc86a0cfdf16b77cb871f54f03e39e3ff70216c9
Michael <michaeljin.lai@gmail.com>
2015-11-19 22:36:26 +1100

    Relocate to index.php when logging out

--- logout.php
+++ logout.php
@@ -7,5 +7,5 @@
     unset($_SESSION['user']); 
      
     // We redirect them to the login page 
-    header("Location: login.php"); 
-    die("Redirecting to: login.php");
+    header("Location: index.php"); 
+    die("Redirecting to: index.php");
