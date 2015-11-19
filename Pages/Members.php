ea44d31379bc32d86f287e00de245cb7ec846cba
Michael <michaeljin.lai@gmail.com>
2015-11-19 22:23:07 +1100

    Allow non logged in users to access Members.php

--- Pages/Members.php
+++ Pages/Members.php
@@ -3,17 +3,6 @@
     // First we execute our common code to connection to the database and start the session 
     require($_SERVER['DOCUMENT_ROOT']."/clashofclans/database.php"); 
      
-    // At the top of the page we check to see whether the user is logged in or not 
-    if(empty($_SESSION['user'])) 
-    { 
-        // If they are not, we redirect them to the login page. 
-        header("Location: login.php"); 
-         
-        // Remember that this die statement is absolutely critical.  Without it, 
-        // people can view your members-only content without logging in. 
-        die("Redirecting to login.php"); 
-    } 
-     
     // Everything below this point in the file is secured by the login system 
      
     // We can retrieve a list of members from the database using a SELECT query. 
