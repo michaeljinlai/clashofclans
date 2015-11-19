9ecc2940f66b8e5c752bd80fda37ca3956f3a26d
Michael <michaeljin.lai@gmail.com>
2015-11-19 22:12:51 +1100

    Change front page to not require login access

--- index.php
+++ index.php
@@ -1,9 +1,41 @@
-<?php require($_SERVER['DOCUMENT_ROOT']."/clashofclans/Elements/header.php"); ?>
+<?php 
 
-<body>
-    <div class="container">
-    	<h1>Placeholder Homepage</h1>
+// First we execute our common code to connection to the database and start the session 
+require($_SERVER['DOCUMENT_ROOT']."/clashofclans/database.php"); 
 
-    	<p><a href="login.php">Login Page<a></p>
-    </div>
-</body>
+?> 
+
+<?php require($_SERVER['DOCUMENT_ROOT']."/clashofclans/Elements/sidebar.php"); ?>
+
+<div class="sidebar-toggle">
+  <!-- This Menu Button is active when side menu is not open -->
+  <div class="">
+    <i class="icon-menu glyphicon glyphicon-triangle-right sidebar-arrow"></i>
+  </div>
+
+  <!-- This Menu Button is active when side menu is open -->
+  <div class="">
+    <i class="icon-menu-open hide glyphicon glyphicon-triangle-right gly-rotate-180 sidebar-arrow"></i>
+  </div>
+
+</div>
+
+<div class="main" id="main">
+  <div>
+      <h1 class="page-header">Home</h1>
+      <div>Hello <?php echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8'); ?>, secret content!</div><br /> 
+      <a href="memberlist.php">Memberlist</a><br /> 
+      <a href="chat/" onclick="openWindow(this.href);this.blur();return false;">Chat</a>
+  </div>
+</div>
+
+<!-- Calculates the width of sidebar everytime the window is resized and adjusts the body -->
+<script>
+$(document).ready(function(){
+    $(window).resize(function(){
+        $('body').css('width', '100%').css('width', '-='+$('.sidebar').width());
+    });
+});
+</script>
+
+
