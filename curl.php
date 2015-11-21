d8afb3dc6289343b33b94424aafb05fc2ea28d26
Michael <michaeljin.lai@gmail.com>
2015-11-21 16:56:33 +1100

    Add new sql file with members table

--- curl.php
+++ curl.php
@@ -6,25 +6,32 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
 // Used for debugging purposes
-if(curl_exec($ch) === false)
-{
-    echo 'Curl error: ' . curl_error($ch);
-}
-else
-{
-    echo 'Operation completed without any errors';
-}
-
+// if(curl_exec($ch) === false)
+// {
+//     echo 'Curl error: ' . curl_error($ch);
+// }
+// else
+// {
+//     echo 'Operation completed without any errors';
+// }
 
 $contents = curl_exec($ch);
+
 // Close handle
 curl_close($ch);
 
 // Can output the contents like this
 //echo $contents;
 
+//convert json object to php associative array
+$data = json_decode($contents, true);
 
+// Echos the first member of memberList
+// echo $data['clanDetails']['results']['memberList'][0]['name'];
 
-
+// Echos the name of each member
+// foreach ($data['clanDetails']['results']['memberList'] as $member) {
+// 	echo $member['name'].'<br>';
+// }
 
 ?>
