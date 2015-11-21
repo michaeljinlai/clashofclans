b319476c072cf9463ee866890e676d835827096a
Michael <michaeljin.lai@gmail.com>
2015-11-21 17:18:36 +1100

    Add the ability to grab members from clash api

--- curl.php
+++ curl.php
@@ -1,4 +1,12 @@
 <?php
+
+require($_SERVER['DOCUMENT_ROOT']."/clashofclans/database.php"); 
+
+// Completely clear table 'members'
+$truncate = "TRUNCATE TABLE members";
+$stmt = $db->prepare($truncate); 
+$result = $stmt->execute(); 
+
 // Create a curl handle to a non-existing location
 $ch = curl_init('https://set7z18fgf.execute-api.us-east-1.amazonaws.com/prod/?route=getClanDetails&clanTag=%232J0G90RR');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -20,18 +28,65 @@ $contents = curl_exec($ch);
 // Close handle
 curl_close($ch);
 
-// Can output the contents like this
-//echo $contents;
-
 //convert json object to php associative array
 $data = json_decode($contents, true);
 
+// Example
 // Echos the first member of memberList
 // echo $data['clanDetails']['results']['memberList'][0]['name'];
 
 // Echos the name of each member
-// foreach ($data['clanDetails']['results']['memberList'] as $member) {
-// 	echo $member['name'].'<br>';
-// }
+foreach ($data['clanDetails']['results']['memberList'] as $member) {
+
+	$query = " 
+	    INSERT INTO members ( 
+	        name, 
+	        role, 
+	        expLevel, 
+	        trophies,
+	        clanRank,
+	        previousClanRank,
+	        donations,
+	        donationsReceived 
+	    ) VALUES ( 
+	        :name, 
+	        :role, 
+	        :expLevel, 
+	        :trophies,
+	        :clanRank,
+	        :previousClanRank,
+	        :donations,
+	        :donationsReceived 
+	    ) 
+	"; 
+
+	$query_params = array( 
+	    ':name' => $member['name'], 
+	    ':role' => $member['role'], 
+	    ':expLevel' => $member['expLevel'], 
+	    ':trophies' => $member['trophies'], 
+	    ':clanRank' => $member['clanRank'], 
+	    ':previousClanRank' => $member['previousClanRank'], 
+	    ':donations' => $member['donations'], 
+	    ':donationsReceived' => $member['donationsReceived'] 
+	); 
+	 
+	try 
+	{ 
+	    // Execute the query to create the user 
+	    $stmt = $db->prepare($query); 
+	    $result = $stmt->execute($query_params); 
+	} 
+	catch(PDOException $ex) 
+	{ 
+	    // Note: On a production website, you should not output $ex->getMessage(). 
+	    // It may provide an attacker with helpful information about your code.  
+	    die("Failed to run query: " . $ex->getMessage()); 
+	} 
+
+}
+
+header("Location: index.php"); 
+die(); 
 
 ?>
