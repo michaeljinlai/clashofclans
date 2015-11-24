c042c30178ae5335f7d2e2989a7dda746caedc18
michaeljin.lai@gmail.com <michaeljin.lai@gmail.com>
2015-11-25 00:11:36 +1100

    Update wardetails tabs

--- Pages/WarDetails.php
+++ Pages/WarDetails.php
@@ -11,26 +11,26 @@
 
   <h1 class="page-header">War Details</h2>
   <ul class="nav nav-pills">
-    <li class="active"><a data-toggle="pill" href="#home">Home</a></li>
-    <li><a data-toggle="pill" href="#menu1">Menu 1</a></li>
-    <li><a data-toggle="pill" href="#menu2">Menu 2</a></li>
-    <li><a data-toggle="pill" href="#menu3">Menu 3</a></li>
+    <li class="active"><a data-toggle="pill" href="#warStats">War Stats</a></li>
+    <li><a data-toggle="pill" href="#warEvents">War Events</a></li>
+    <li><a data-toggle="pill" href="#myTeam">My Team</a></li>
+    <li><a data-toggle="pill" href="#enemyTeam">Enemy Team</a></li>
   </ul>
   
   <div class="tab-content">
-    <div id="home" class="tab-pane fade in active">
+    <div id="warStats" class="tab-pane fade in active">
       <h3>HOME</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </div>
-    <div id="menu1" class="tab-pane fade">
+    <div id="warEvents" class="tab-pane fade">
       <h3>Menu 1</h3>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </div>
-    <div id="menu2" class="tab-pane fade">
+    <div id="myTeam" class="tab-pane fade">
       <h3>Menu 2</h3>
       <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
     </div>
-    <div id="menu3" class="tab-pane fade">
+    <div id="enemyTeam" class="tab-pane fade">
       <h3>Menu 3</h3>
       <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
     </div>
