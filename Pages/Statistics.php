4a822478127f3ef3efb919c8f710c7a3ac3dc498
Michael <michaeljin.lai@gmail.com>
2015-11-24 14:12:31 +1100

    Update Statistics Page

--- Pages/Statistics.php
+++ Pages/Statistics.php
@@ -1,3 +1,272 @@
+<?php 
+
+    // First we execute our common code to connection to the database and start the session 
+    require($_SERVER['DOCUMENT_ROOT']."/clashofclans/database.php"); 
+     
+    // Everything below this point in the file is secured by the login system 
+     
+    // We can retrieve a list of members from the database using a SELECT query. 
+    // In this case we do not have a WHERE clause because we want to select all 
+    // of the rows from the database table. 
+    $query = " 
+        SELECT 
+            name, 
+            role, 
+            expLevel,
+            trophies,
+            clanRank,
+            previousClanRank,
+            donations,
+            donationsReceived,
+            leagueBadgeImg_s,
+            leagueBadgeImg_xl 
+        FROM members 
+    "; 
+     
+    try 
+    { 
+        // These two statements run the query against your database table. 
+        $stmt = $db->prepare($query); 
+        $stmt->execute(); 
+    } 
+    catch(PDOException $ex) 
+    { 
+        // Note: On a production website, you should not output $ex->getMessage(). 
+        // It may provide an attacker with helpful information about your code.  
+        die("Failed to run query: " . $ex->getMessage()); 
+    } 
+         
+    // Finally, we can retrieve all of the found rows into an array using fetchAll 
+    $rows = $stmt->fetchAll(); 
+?> 
+
+<?php require($_SERVER['DOCUMENT_ROOT']."/clashofclans/Elements/header.php"); ?>
+
+<?php // Testing Area
+  
+  // Initialize variables
+  $leaderCount = 0;
+  $coLeaderCount = 0;
+  $adminCount = 0;
+  $memberCount = 0;
+
+  foreach ($rows as $row) {
+    switch ($row['role']) {
+      case 'leader':
+        $leaderCount = $leaderCount + 1;
+        break;
+      case 'coLeader':
+        $coLeaderCount = $coLeaderCount + 1;
+        break;
+      case 'admin':
+        $adminCount = $adminCount + 1;
+        break;
+      case 'member':
+        $memberCount = $memberCount + 1;
+        break;
+    }
+  }
+
+  $total = $leaderCount+$coLeaderCount+$adminCount+$memberCount;
+
+?>
+
+<style type="text/css">
+@import url(http://fonts.googleapis.com/css?family=Open+Sans:400,700);
+@keyframes bake-pie {
+  from {
+    transform: rotate(0deg) translate3d(0, 0, 0);
+  }
+}
+/*body {
+  font-family: "Open Sans", Arial;
+  background: #EEE;
+}*/
+
+main {
+  width: 400px;
+  margin: 30px auto;
+}
+
+section {
+  margin-top: 30px;
+}
+
+.pieID {
+  display: inline-block;
+  vertical-align: top;
+}
+
+.pie {
+  height: 200px;
+  width: 200px;
+  position: relative;
+  margin: 0 30px 30px 0;
+}
+
+.pie::before {
+  content: "";
+  display: block;
+  position: absolute;
+  z-index: 1;
+  width: 100px;
+  height: 100px;
+  background: #EEE;
+  border-radius: 50%;
+  top: 50px;
+  left: 50px;
+}
+
+.pie::after {
+  content: "";
+  display: block;
+  width: 120px;
+  height: 2px;
+  background: rgba(0, 0, 0, 0.1);
+  border-radius: 50%;
+  box-shadow: 0 0 3px 4px rgba(0, 0, 0, 0.1);
+  margin: 220px auto;
+}
+
+.slice {
+  position: absolute;
+  width: 200px;
+  height: 200px;
+  clip: rect(0px, 200px, 200px, 100px);
+  animation: bake-pie 1s;
+}
+
+.slice span {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: black;
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  clip: rect(0px, 200px, 200px, 100px);
+}
+
+.legend {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  background: #FFF;
+  padding: 15px;
+  font-size: 13px;
+  box-shadow: 1px 1px 0 #DDD, 2px 2px 0 #BBB;
+}
+
+.legend li {
+  width: 110px;
+  height: 1.25em;
+  margin-bottom: 0.7em;
+  padding-left: 0.5em;
+  border-left: 1.25em solid black;
+}
+
+.legend em {
+  font-style: normal;
+}
+
+.legend span {
+  float: right;
+}
+
+</style>
+
 <div>
     <h1 class="page-header">Statistics</h1>
 </div>
+
+<main>
+  <h1>Title</h1>
+  <p>description</p>
+  <section>
+    <div class="pieID pie">
+      
+    </div>
+    <ul class="pieID legend">
+      <li>
+        <em>Humans</em>
+        <span>718</span>
+      </li>
+      <li>
+        <em>Dogs</em>
+        <span>531</span>
+      </li>
+      <li>
+        <em>Cats</em>
+        <span>868</span>
+      </li>
+      <li>
+        <em>Slugs</em>
+        <span>344</span>
+      </li>
+      <li>
+        <em>Aliens</em>
+        <span>1145</span>
+      </li>
+    </ul>
+  </section>
+</main>
+
+<script type="text/javascript">
+function sliceSize(dataNum, dataTotal) {
+  return (dataNum / dataTotal) * 360;
+}
+function addSlice(sliceSize, pieElement, offset, sliceID, color) {
+  $(pieElement).append("<div class='slice "+sliceID+"'><span></span></div>");
+  var offset = offset - 1;
+  var sizeRotation = -179 + sliceSize;
+  $("."+sliceID).css({
+    "transform": "rotate("+offset+"deg) translate3d(0,0,0)"
+  });
+  $("."+sliceID+" span").css({
+    "transform"       : "rotate("+sizeRotation+"deg) translate3d(0,0,0)",
+    "background-color": color
+  });
+}
+function iterateSlices(sliceSize, pieElement, offset, dataCount, sliceCount, color) {
+  var sliceID = "s"+dataCount+"-"+sliceCount;
+  var maxSize = 179;
+  if(sliceSize<=maxSize) {
+    addSlice(sliceSize, pieElement, offset, sliceID, color);
+  } else {
+    addSlice(maxSize, pieElement, offset, sliceID, color);
+    iterateSlices(sliceSize-maxSize, pieElement, offset+maxSize, dataCount, sliceCount+1, color);
+  }
+}
+function createPie(dataElement, pieElement) {
+  var listData = [];
+  $(dataElement+" span").each(function() {
+    listData.push(Number($(this).html()));
+  });
+  var listTotal = 0;
+  for(var i=0; i<listData.length; i++) {
+    listTotal += listData[i];
+  }
+  var offset = 0;
+  var color = [
+    "cornflowerblue", 
+    "olivedrab", 
+    "orange", 
+    "tomato", 
+    "crimson", 
+    "purple", 
+    "turquoise", 
+    "forestgreen", 
+    "navy", 
+    "gray"
+  ];
+  for(var i=0; i<listData.length; i++) {
+    var size = sliceSize(listData[i], listTotal);
+    iterateSlices(size, pieElement, offset, i, 0, color[i]);
+    $(dataElement+" li:nth-child("+(i+1)+")").css("border-color", color[i]);
+    offset += size;
+  }
+}
+createPie(".pieID.legend", ".pieID.pie");
+
+</script>
