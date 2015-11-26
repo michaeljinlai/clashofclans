caf67334c97e81736d57df953c33cfc3f59f0b0d
Michael <michaeljin.lai@gmail.com>
2015-11-26 17:31:36 +1100

    Update war log table to auto get data from json

--- Pages/WarLog.php
+++ Pages/WarLog.php
@@ -3,6 +3,14 @@
 <!-- Datatables CSS -->
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/s/bs/dt-1.10.10/datatables.min.css"/> 
 
+<?php
+	// Gets an array of all json file names (including '.' and '..')
+	$directory = '../database/war-history/json';
+
+	// Remove the '.' and '..' from the array
+	$scanned_directory = array_diff(scandir($directory), array('..', '.'));
+?>
+
 <div id="warMain">
 	<div class="table-responsive">
 	    <h1 class="page-header">War Log</h1>
@@ -20,34 +28,25 @@
 				</tr>
 			</thead>
 			<tbody>
-				<tr>
-					<td>2d ago</td>
-					<td>50%</td>
-					<td>star</td>
-					<td>Prepare to Die</td>
-					<td>Enemy Clan Name</td>
-					<td>star</td>
-					<td>50%</td>
-					<td><a onclick="loadWar('23432')" class="btn btn-primary">Details</a></td>
-				</tr>
+				<?php foreach ($scanned_directory as $warFile) : ?>
+				<?php 
+					$str = file_get_contents('../database/war-history/json/'.$warFile);
+					$json = json_decode($str, true);
+				?>
+					<tr>
+						<td>Unknown</td>
+						<td><?php echo $json['summary']['home']['totalDestruction']; ?></td>
+						<td><?php echo ($json['summary']['home']['3Star']*3 + $json['summary']['home']['2Star']*2 + $json['summary']['home']['1Star']); ?></td>
+						<td>Prepare to Die</td>
+						<td><?php echo $json['enemy']['name']; ?></td>
+						<td><?php echo ($json['summary']['enemy']['3Star']*3 + $json['summary']['enemy']['2Star']*2 + $json['summary']['enemy']['1Star']); ?></td>
+						<td><?php echo $json['summary']['enemy']['totalDestruction']; ?></td>
+						<td><a onclick="loadWar('23432')" class="btn btn-primary">Details</a></td>
+					</tr>
+				<?php endforeach; ?>
 			</tbody>
 		</table>
 	</div>
-</div>
-
-<div>
-	<h2>Test Section</h2>
-	<?php
-		$directory = '../database/war-history/json';
-
-		$scanned_directory = array_diff(scandir($directory), array('..', '.'));
-
-		foreach ($scanned_directory as $warFile) {
-			$str = file_get_contents('../database/war-history/json/'.$warFile);
-			$json = json_decode($str, true);
-			echo $json['summary']['enemy']['totalDestruction'].'<br>';
-		}
-	?>
 </div>
 
 <script type="text/javascript">
