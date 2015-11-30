8bdb91dbaa90ffc59c12166e76bf71e73da481a7
Michael <michaeljin.lai@gmail.com>
2015-11-30 20:38:12 +1100

    Stop visiting users by url when not admin

--- Pages/Users.php
+++ Pages/Users.php
@@ -3,6 +3,17 @@
     // First we execute our common code to connection to the database and start the session 
     require($_SERVER['DOCUMENT_ROOT']."/clashofclans/database.php"); 
      
+    // Check if user is logged in
+    if(empty($_SESSION['user']) || $_SESSION['user']['privilege'] !== 'administrator') 
+    { 
+        // If they are not, we redirect them to the login page. 
+        header("Location: ../login.php"); 
+
+        // Remember that this die statement is absolutely critical.  Without it, 
+        // people can view your members-only content without logging in. 
+        die("Redirecting to login.php"); 
+    } 
+     
     // Everything below this point in the file is secured by the login system 
      
     // We can retrieve a list of members from the database using a SELECT query. 
