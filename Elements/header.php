a26e2e83bf5dea8ee6534b09d835663a33ac3aba
Michael <michaeljin.lai@gmail.com>
2015-11-30 23:50:58 +1100

    Add favicon

--- Elements/header.php
+++ Elements/header.php
@@ -5,4 +5,5 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
+    <link rel='shortcut icon' href='img/favicon.ico' type='image/x-icon'/ >
 </head>
