1c5a4f3a951348ac87c15e6ff967f5615409d4c5
yschua <yiesheng.chua@gmail.com>
2015-12-03 17:21:28 +1100

    Remove comments from index.php, sidebar.php only
    Too hard to use/write a script..

--- Elements/sidebar.php
+++ Elements/sidebar.php
@@ -1,16 +1,11 @@
 <?php require($_SERVER['DOCUMENT_ROOT']."/clashofclans/Elements/header.php"); ?>
 <html>
 
-<!-- Mobile device options -->
-<!-- <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;"> -->
-
 <div class="sidebar">
 	<ul class="nav nav-sidebar">
-		<!-- Logo -->
 		<li>
 			<img class="ptd-logo" src="img/ptd.png" />
 		</li>
-		<!-- Logo -->
 		<div class="sidebar-line">
 
 		</div>
@@ -38,7 +33,6 @@
 				<span class="sidebar-text">Statistics</span>
 			</a>
 		</li>
-		<!-- Only show Profile button if they are logged in -->
 		<?php 
 		if(!empty($_SESSION['user'])) { echo '
 			<li>
@@ -48,7 +42,6 @@
 				</a>
 			</li>';}
 		?>
-		<!-- Only show User button if the user is an administrator -->
 		<?php 
 		if(!empty($_SESSION['user']) && $_SESSION['user']['privilege'] === 'administrator') {echo '
 			<li>
@@ -58,7 +51,6 @@
 				</a>
 			</li>';}
 		?>
-		<!-- Only show Profile button if the user is an administrator -->
 		<?php 
 		if(!empty($_SESSION['user']) && $_SESSION['user']['privilege'] === 'administrator') { echo '
 			<li>
@@ -68,7 +60,6 @@
 				</a>
 			</li>';}
 		?>
-		<!-- Only show Profile button if the user is an administrator -->
 		<?php 
 		if(!empty($_SESSION['user']) && $_SESSION['user']['privilege'] === 'administrator') { echo '
 			<li>
@@ -87,7 +78,6 @@
 				</a>
 			</li>';}
 		?>
-		<!-- Only show log out button if they are logged in -->
 		<?php 
 		if(!empty($_SESSION['user'])) { echo '
 			<li>
@@ -98,7 +88,6 @@
 			</li>';
 		}
 		?>
-		<!-- Only show log in button if they are not logged in -->		
 		<?php 
 		if(empty($_SESSION['user'])) { echo '
 			<li class="sidebar-log-in">
@@ -117,62 +106,45 @@
 <script src="js/app.js"></script>
 <script src="js/bootstrap.min.js"></script>
 
-<!-- So the Tooltips show on top of the body container -->
 <script>
 $(function () {
 	$('[data-toggle="tooltip"]').tooltip({container: 'body'});
 });
 </script>
 
-<!-- DataTables -->
 <script id="testTwo" type="text/javascript" charset="utf8" src="https://cdn.datatables.net/s/bs/dt-1.10.10/datatables.min.js"></script>
 
-<!-- Sidebar Button Tab Changes -->
 <script>
 function loadDoc(str) {
 		$("#main").load("Pages/"+str+".php", function() {
-	    // This gets executed when the content is loaded
-	    //$.get("//code.jquery.com/jquery-1.10.2.min.js");
-	    //$.get("//cdn.datatables.net/1.10.10/js/jquery.dataTables.js");
 	});
 }
 </script>
 
-<!-- Sidebar Delete Button -->
-<!-- For some reasons I can't get this to work when the two delete php files are in the Pages folder -->
 <script>
 function loadDelete(str) {
 		$("#main").load(str+".php", function() {
-	    // This gets executed when the content is loaded
-	    //$.get("//code.jquery.com/jquery-1.10.2.min.js");
-	    //$.get("//cdn.datatables.net/1.10.10/js/jquery.dataTables.js");
 	});
 }
 </script>
 
-<!-- War Log Details Changes -->
 <script>
 function loadWar(id) {
 		$("#warMain").load("Pages/WarDetails.php?id="+id, function() {
-	    // Code here gets executed when the content is loaded
 	});
 }
 </script>
 
-<!-- Active Class when sidebar is clicked -->
 <script>
 $(function () {
 	$('.nav li a').click(function (e) {
-    //e.preventDefault(); // For some reason this line prevents the logout button the the sidebar from working
     $('.nav li a').removeClass('active');
     $(this).closest('a').addClass('active');
 });
 });
 </script>
 
-<!-- Makes Chat Popout -->
 <script type="text/javascript">
-  // <![CDATA[
     function openWindow(url,width,height,options,name) {
       width = width ? width : 800;
       height = height ? height : 600;
@@ -184,10 +156,8 @@ $(function () {
         'screenX='+(screen.width-width)/2+',screenY='+(screen.height-height)/2+',width='+width+',height='+height+','+options
       )
     }
-  // ]]>
 </script>
 
-<!-- Google Analytics -->
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -199,7 +169,6 @@ $(function () {
 
 </script>
 
-<!-- Highcharts -->
 <script src="js/highcharts.js"></script>
 <script src="js/modules/data.js"></script>
 <script src="js/modules/exporting.js"></script>
--- index.php
+++ index.php
@@ -1,30 +1,22 @@
 <?php 
-
-// First we execute our common code to connection to the database and start the session 
 require($_SERVER['DOCUMENT_ROOT']."/clashofclans/database.php"); 
-
 ?> 
 
 <?php require($_SERVER['DOCUMENT_ROOT']."/clashofclans/Elements/sidebar.php"); ?>
 
 <div class="sidebar-toggle">
-  <!-- This Menu Button is active when side menu is not open -->
   <div class="">
     <i class="icon-menu hide glyphicon glyphicon-triangle-right sidebar-arrow"></i>
   </div>
 
-  <!-- This Menu Button is active when side menu is open -->
   <div class="">
     <i class="icon-menu-open glyphicon glyphicon-triangle-right gly-rotate-180 sidebar-arrow"></i>
   </div>
 
 </div>
 
-<div class="main" id="main">
+<div class="main" id="main"></div>
 
-</div>
-
-<!-- Calculates the width of sidebar everytime the window is resized and adjusts the body -->
 <script>
 $(document).ready(function(){
 
@@ -32,10 +24,8 @@ $(document).ready(function(){
         $('body').css('width', '100%').css('width', '-='+$('.sidebar').width());
     });
 
-    // Both of these will be loaded once the page initally loads
     $("#main").load("Pages/Home.php", function() {});
     $("[data-toggle='tooltip']").tooltip('destroy');
-
 });
 </script>
 
