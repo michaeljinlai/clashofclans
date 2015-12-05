a1fe124071872f8d6c828a26fb4bdc36584c8f00
Michael <michaeljin.lai@gmail.com>
2015-12-06 01:24:10 +1100

    Update test file

--- test.php
+++ test.php
@@ -8,6 +8,8 @@ $scanned_directory = array_diff(scandir($directory), array('..', '.'));
 
 $playerArray = array();
 $attacksUsedArray = array();
+$starsEarnedArray = array();
+$totalDamageArray = array(); 
 
 foreach ($scanned_directory as $warFile) {
 	$str = file_get_contents('database/war-history/json/'.$warFile);
@@ -25,31 +27,41 @@ foreach ($scanned_directory as $warFile) {
 
 for ($i = 0; $i < count($playerArray); $i++) {
 	$attacksUsedArray[$i] = 0;
+	$starsEarnedArray[$i] = 0;
+	$totalDamageArray[$i] = 0;
 }
 
-$fullArray = array($playerArray, $attacksUsedArray);
+$fullArray = array($playerArray, $attacksUsedArray, $starsEarnedArray, $totalDamageArray);
 
 // Legend
 // $fullArray[0] List of all player names
-// $fullArray[0] List of total stars earned
+// $fullArray[1] Total attacks
+// $fullArray[2] Stars earned
 
 foreach ($scanned_directory as $warFile) {
 	$str = file_get_contents('database/war-history/json/'.$warFile);
 	$json = json_decode($str, true);
 
 	foreach ($json['home']['roster'] as $roster) {
-
-        // Put all attacks used
+        // Put all data in array
         $count = 0;
-        foreach ($fullArray[0] as $wat) {
-	        if ($wat === $roster['name']) {
-	        	$fullArray[1][$count] = $fullArray[1][$count] + 5;
+        foreach ($fullArray[0] as $player) {
+	        if ($player === $roster['name']) {
+	        	if ($roster['attack1']['target'] !== "") {
+	        		$fullArray[1][$count] = $fullArray[1][$count] + 1;
+	        		$fullArray[2][$count] = $fullArray[2][$count] + $roster['attack1']['starsEarned'];
+	        		$fullArray[3][$count] = $fullArray[3][$count] + $roster['attack1']['damage'];
+	        	}
+	        	if ($roster['attack2']['target'] !== "") {
+	        		$fullArray[1][$count] = $fullArray[1][$count] + 1;
+	        		$fullArray[2][$count] = $fullArray[2][$count] + $roster['attack2']['starsEarned'];
+	        		$fullArray[3][$count] = $fullArray[3][$count] + $roster['attack2']['damage'];
+	        	}
 	        }
 	        $count = $count + 1;
 	    }
 	}
 }	
-
 
 var_dump($fullArray);
 
