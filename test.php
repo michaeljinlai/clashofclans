37587e78a0ea2f722956fbe07800c206fdae013e
Michael <michaeljin.lai@gmail.com>
2015-12-06 01:29:01 +1100

    Update test file

--- test.php
+++ test.php
@@ -9,6 +9,7 @@ $scanned_directory = array_diff(scandir($directory), array('..', '.'));
 $playerArray = array();
 $attacksUsedArray = array();
 $starsEarnedArray = array();
+$totalStarsWonArray = array(); 
 $totalDamageArray = array(); 
 
 foreach ($scanned_directory as $warFile) {
@@ -28,15 +29,18 @@ foreach ($scanned_directory as $warFile) {
 for ($i = 0; $i < count($playerArray); $i++) {
 	$attacksUsedArray[$i] = 0;
 	$starsEarnedArray[$i] = 0;
+	$totalStarsWonArray[$i] = 0;
 	$totalDamageArray[$i] = 0;
 }
 
-$fullArray = array($playerArray, $attacksUsedArray, $starsEarnedArray, $totalDamageArray);
+$fullArray = array($playerArray, $attacksUsedArray, $starsEarnedArray, $totalStarsWonArray, $totalDamageArray);
 
 // Legend
 // $fullArray[0] List of all player names
 // $fullArray[1] Total attacks
 // $fullArray[2] Stars earned
+// $fullArray[3] Stars won
+// $fullArray[4] Total damage
 
 foreach ($scanned_directory as $warFile) {
 	$str = file_get_contents('database/war-history/json/'.$warFile);
@@ -50,12 +54,14 @@ foreach ($scanned_directory as $warFile) {
 	        	if ($roster['attack1']['target'] !== "") {
 	        		$fullArray[1][$count] = $fullArray[1][$count] + 1;
 	        		$fullArray[2][$count] = $fullArray[2][$count] + $roster['attack1']['starsEarned'];
-	        		$fullArray[3][$count] = $fullArray[3][$count] + $roster['attack1']['damage'];
+	        		$fullArray[3][$count] = $fullArray[3][$count] + $roster['attack1']['starsWon'];
+	        		$fullArray[4][$count] = $fullArray[4][$count] + $roster['attack1']['damage'];
 	        	}
 	        	if ($roster['attack2']['target'] !== "") {
 	        		$fullArray[1][$count] = $fullArray[1][$count] + 1;
 	        		$fullArray[2][$count] = $fullArray[2][$count] + $roster['attack2']['starsEarned'];
-	        		$fullArray[3][$count] = $fullArray[3][$count] + $roster['attack2']['damage'];
+	        		$fullArray[3][$count] = $fullArray[3][$count] + $roster['attack2']['starsWon'];
+	        		$fullArray[4][$count] = $fullArray[4][$count] + $roster['attack2']['damage'];
 	        	}
 	        }
 	        $count = $count + 1;
@@ -64,27 +70,6 @@ foreach ($scanned_directory as $warFile) {
 }	
 
 var_dump($fullArray);
-
-
-
-
-
-// $array1 = array("ben", "tom", "james");
-// $array2 = array("1", "2", "3");
-// $array3 = array($array1, $array2);
-
-// print_r($array3);
-// echo '<br>';
-
-// $count = 0;
-// foreach ($array3[0] as $wat) {
-// 	if ($wat === "tom") {
-// 		$array3[1][$count] = $array3[1][$count] + 5;
-// 	}
-// 	$count = $count + 1;
-// }
-
-// print_r($array3);
 
 ?>
 
