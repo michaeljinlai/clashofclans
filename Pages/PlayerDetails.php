82e0261c0817cd325d4e1749dde1a7be404f98e3
Michael <michaeljin.lai@gmail.com>
2015-12-06 19:47:33 +1100

    Change details to statistics

--- Pages/PlayerDetails.php
+++ Pages/PlayerDetails.php
@@ -1,6 +1,6 @@
 <div class="enter-effect">
 
-	<h1 class="page-header">Player Details <small><?php echo '('.$_GET['name'].')'; ?></small></h1>
+	<h1 class="page-header">Player Statistics <small><?php echo '('.$_GET['name'].')'; ?></small></h1>
 	<ol class="breadcrumb">
 	    <li><a href="" onClick="loadDoc('Home'); return false;">Home</a></li>
 	    <li><a href="" onClick="loadDoc('Statistics'); return false;">Statistics</a></li>
